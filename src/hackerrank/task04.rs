767efc059da094dae98954297e68a65131fedf30
oleshkoanton-star <oleshko.anton@e-u.edu.ua>
2026-04-14 10:33:26 +0300

    task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,41 @@
 // https://www.hackerrank.com/challenges/grading/problem
+use std::io;
+
+fn grading_students(grades: Vec<i32>) -> Vec<i32> {
+    grades
+        .into_iter()
+        .map(|grade| {
+            if grade < 38 {
+                grade
+            } else {
+                let next_multiple = ((grade / 5) + 1) * 5;
+                if next_multiple - grade < 3 {
+                    next_multiple
+                } else {
+                    grade
+                }
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    let mut grades = Vec::new();
+
+    for _ in 0..n {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        let grade: i32 = input.trim().parse().unwrap();
+        grades.push(grade);
+    }
+
+    let result = grading_students(grades);
+
+    for grade in result {
+        println!("{}", grade);
+    }
+}
