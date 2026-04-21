9c6e74e151fbde68304ca0ca52a350cf415e686c
oleshkoanton-star <oleshko.anton@e-u.edu.ua>
2026-04-21 13:17:54 +0300

    Update task08.rs

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,41 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+use std::io;
+
+fn breaking_records(scores: Vec<i32>) -> (i32, i32) {
+    let mut min_score = scores[0];
+    let mut max_score = scores[0];
+
+    let mut min_count = 0;
+    let mut max_count = 0;
+
+    for &score in &scores[1..] {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
+        }
+    }
+
+    (max_count, min_count)
+}
+
+fn main() {
+    let mut input = String::new();
+
+    // n (можно не использовать)
+    io::stdin().read_line(&mut input).unwrap();
+    input.clear();
+
+    // scores
+    io::stdin().read_line(&mut input).unwrap();
+    let scores: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let (max_count, min_count) = breaking_records(scores);
+
+    println!("{} {}", max_count, min_count);
+}
